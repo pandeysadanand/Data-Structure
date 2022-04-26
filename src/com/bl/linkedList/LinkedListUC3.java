74dc9a1cf33b9071e315a8dbb046a3b85f0778e3
sadanand pandey <pandeysadanad75@gmail.com>
2022-04-26 08:17:36 +0530

    Added: appending values

---
+++ src/com/bl/linkedList/LinkedListUC3.java
@@ -0,0 +1,22 @@
+package com.bl.linkedList;
+
+import java.util.Scanner;
+import java.util.*;
+
+/*
+ * @author : Sadanand Pandey
+ * creating linked list by appending
+ */
+public class LinkedListUC3 {
+
+	public static void main(String[] args) {
+		/*
+		 * creating empty linked list object and appending value to object
+		 */
+		List list = new LinkedList();
+		list.add(56);
+		list.add(30);
+		list.add(70);
+		System.out.println(list);
+	}
+}
