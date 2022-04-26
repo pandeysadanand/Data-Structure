dbc6d17a43032f1c902356146677200b18b3052a
sadanand pandey <pandeysadanad75@gmail.com>
2022-04-26 07:58:02 +0530

    Added: creating linked list by adding node

--- src/com/bl/linkedList/LinkedList.java
+++ src/com/bl/linkedList/LinkedList.java
@@ -1,23 +1,81 @@
 package com.bl.linkedList;
 
-/*
- * @author: sadanand pandey
- * creating simple linked list
- */
-import java.util.*;
+import java.util.Scanner;
 
 public class LinkedList {
+
+	Node head;
+
+	/*
+	 * Linked list Node. This inner class is made static so that main() can access
+	 * it
+	 */
+
+	static class Node {
+		int data;
+		Node next;
+
+		// constructor
+		Node(int data) {
+			this.data = data;
+			this.next = null;
+		}
+	}
+
+	/*
+	 * method to insert new node
+	 */
+	public static LinkedList insert(LinkedList list, int data) {
+		/* creating new node */
+		Node newNode = new Node(data);
+		newNode.next = null;
+		/*
+		 * if new node is empty, thenb new node as head
+		 */
+		if (list.head == null) {
+			list.head = newNode;
+		} else {
+			/*
+			 * Else traverse till the last node and insert the new node 
+			 */
+			Node tail = list.head;
+			while (tail.next != null) {
+				tail = tail.next;
+			}
+			// insert new node at last
+			tail.next = newNode;
+		}
+		return list;
+	}
+
+	/*
+	 * method to print the linkedList
+	 */
+	public static void display(LinkedList list) {
+		Node currNode = list.head;
+		System.out.println("---------------------");
+		System.out.print("LinkedList: ");
+
+		// traverse through linkedList
+		while (currNode != null) {
+			System.out.print(currNode.data + "->");
+			// go to next node
+			currNode = currNode.next;
+		}
+	}
+
 	public static void main(String[] args) {
-		List list = new java.util.LinkedList<>();
-		/*
-		 * Adding number to list
-		 */
-		list.add(0, 56);
-		list.add(1, 30);
-		list.add(2, 70);
-		/*
-		 * printing all element present in list
-		 */
-		System.out.println(list);
+		int number;
+		LinkedList list = new LinkedList();
+		int element;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("How many number you want to insert: \n" );
+		element = sc.nextInt();
+		for(int i = 1; i <= element; i++) {
+			System.out.print("Enter number " + i + " : ");
+			number = sc.nextInt();
+			list = insert(list, number);
+		}
+				display(list);
 	}
 }
