00cfecf6ab57c02b08f331ca08c43360ca13f057
sadanand pandey <pandeysadanad75@gmail.com>
2022-04-27 17:56:03 +0530

    Added: Creating binary tree with given numbers

--- src/com/bl/binarysearchtree/BinaryTree.java
+++ src/com/bl/binarysearchtree/BinaryTree.java
@@ -3,66 +3,105 @@ package com.bl.binarysearchtree;
 import java.util.Scanner;
 
 /*
- * @author :Sadanand pandey 
- * creating BST for inserting node
+ * @author: sadanand pandey 
+ * code for inserting number to BST
  */
-public class BinaryTree {
-	public static void main(String[] args) {
-		// calling run method to perform insertion
-		new BinaryTree().run();
+class BST_class {
+
+	class Node {
+		int key;
+		Node left, right;
+
+		public Node(int data) {
+			key = data;
+			left = right = null;
+		}
 	}
 
 	/*
-	 * represent the node of BST
+	 * BST root node
 	 */
-	static class Node {
-		Node left;
-		Node right;
-		int number;
+	Node root;
+
+	/*
+	 * Constructor for BST =>initial empty tree
+	 */
+	BST_class() {
+		root = null;
+	}
+
+	// insert a node in BST
+	void insert(int key) {
+		root = insert_Recursive(root, key);
+	}
+
+	// recursive insert function
+	Node insert_Recursive(Node root, int key) {
+		/*
+		 * tree is empty
+		 */
+		if (root == null) {
+			root = new Node(key);
+			return root;
+		}
+		/*
+		 * traverse the tree
+		 */
+		if (key < root.key)
+			root.left = insert_Recursive(root.left, key);
+		else if (key > root.key)
+			root.right = insert_Recursive(root.right, key);
+		return root;
+	}
+
+	/*
+	 * method for inorder traversal of BST
+	 */
+	void inorder() {
+		inorder_Recursive(root);
+	}
+
+	// recursively traverse the BST
+	void inorder_Recursive(Node root) {
+		if (root != null) {
+			inorder_Recursive(root.left);
+			System.out.print(root.key + " ");
+			inorder_Recursive(root.right);
+		}
+	}
+
+	// recursive insert function
+	Node search_Recursive(Node root, int key) {
+		if (root == null || root.key == key)
+			return root;
+		if (root.key > key)
+			return search_Recursive(root.left, key);
+		// val is less than root's key
+		return search_Recursive(root.right, key);
+		
+	}
+}
+
+class BinaryTree {
+	public static void main(String[] args) {
+		BST_class bst = new BST_class();
 
 		/*
-		 * created constructor to sset value of number
+		 * insert data into BST
 		 */
-		public Node(int number) {
-			this.number = number;
-		}
-	}
-
-	/*
-	 * in this run() method taking inputs from user and setting to insert() method
-	 */
-	public void run() {
-		Node rootNode = new Node(56);
-		System.out.println("Creating binary tree with root values ");
-		System.out.println("=========================================");
 		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter total number of node of binary tree : ");
-		int element = sc.nextInt();
-		for (int i = 0; i < element; i++) {
-			System.out.println("Enter number : " + i);
+		System.out.print("Enter length of binary tree: ");
+		int length = sc.nextInt();
+		for (int i = 0; i < length; i++) {
+			System.out.println("Enter number: " + i);
 			int number = sc.nextInt();
-			insert(rootNode, number);
+			bst.insert(number);
 		}
-	}
 
-	/*
-	 * inserting number to right position in the tree by comparing each time
-	 */
-	public void insert(Node node, int number) {
-		if (number < node.number) {
-			if (node.left != null) {
-				insert(node.left, number);
-			} else {
-				System.out.println("Inserted " + number + " to left of node " + node.number);
-				node.left = new Node(number);
-			}
-		} else if (number > node.number) {
-			if (node.right != null) {
-				insert(node.right, number);
-			} else {
-				System.out.println("Inserted " + number + " to righ of node " + node.number);
-				node.right = new Node(number);
-			}
-		}
+		/*
+		 * print the BST
+		 */
+		System.out.println("The BST Created with input data(Left-root-right):");
+		bst.inorder();
 	}
 }
